b18db5ff689ebf8ce30f3eb3e988ffa3d1cc9555
AJMifsud <84155033+AJMifsud@users.noreply.github.com>
2023-04-26 02:08:13 +0100

    Update shithead.js

--- shithead.js
+++ shithead.js
@@ -14,6 +14,7 @@ const playerNamesContainer = document.getElementById("playernames-container");
 const playerLeftContainer = document.getElementById("playerleft-container");
 const playerTopContainer = document.getElementById("playertop-container");
 const playerRightContainer = document.getElementById("playerright-container");
+const playerBottomContainer = document.getElementById("playerbottom-container");
 
 function updatePlayerContainers(numPlayers) {
   playerLeftContainer.style.display = "none";
@@ -23,15 +24,20 @@ function updatePlayerContainers(numPlayers) {
   switch (numPlayers) {
     case "2":
       playerTopContainer.style.display = "flex";
+      playerTopContainer.querySelector('.player-name').textContent = 'Player 2';
       break;
     case "3":
       playerLeftContainer.style.display = "flex";
       playerRightContainer.style.display = "flex";
+      playerRightContainer.querySelector('.player-name').textContent = 'Player 3';
       break;
     case "4":
       playerLeftContainer.style.display = "flex";
+      playerLeftContainer.querySelector('.player-name').textContent = 'Player 2';
       playerTopContainer.style.display = "flex";
+      playerTopContainer.querySelector('.player-name').textContent = 'Player 3';
       playerRightContainer.style.display = "flex";
+      playerRightContainer.querySelector('.player-name').textContent = 'Player 4';
       break;
     default:
       console.error("Invalid number of players: " + numPlayers);
@@ -42,9 +48,8 @@ function updatePlayerContainers(numPlayers) {
 numPlayersSelect.addEventListener("change", function () {
   const numPlayers = this.value;
   updatePlayerContainers(numPlayers);
-});
-
-while (playerNamesContainer.firstChild) {
+ 
+  while (playerNamesContainer.firstChild) {
   playerNamesContainer.removeChild(playerNamesContainer.firstChild);
 }
 
@@ -54,8 +59,71 @@ for (let i = 0; i < numPlayersSelect.value; i++) {
   playerNameInput.name = `player${i + 1}`;
   playerNameInput.placeholder = `Player ${i + 1} Name`;
   playerNamesContainer.appendChild(playerNameInput);
+  
+   playerNameInput.addEventListener("input", function() {
+      playerBottomContainer.querySelector('.player-name').textContent = `${playerNameInput.value}`;
+  });
+}
+});
+updatePlayerContainers(numPlayersSelect.value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createDeck() {
+  const suits = ["hearts", "diamonds", "clubs", "spades"];
+  const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+  const deck = [];
+  let withJokers = "";
+switch (withJokers) {
+	case "No":
+    // create cards without jokers
+    for (let suit of suits) {
+      for (let value of values) {
+        deck.push({ value, suit });
+      }
+    }
+	break;
+    case "Yes":
+    // create jokers
+    for (let i = 0; i < 2; i++) {
+      deck.push({ value: "Joker", suit: "joker" });
+    }
+    break;
+  }
+  return deck;
 }
 
-updatePlayerContainers(numPlayersSelect.value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
