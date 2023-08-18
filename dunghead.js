52eb0c92ba65951bb0753f1cf3f2965bfea66382
Andy <milfsud@gmail.com>
2023-08-18 19:06:29 +0100

    Fixed bug needing extra click to play multiple

--- dunghead.js
+++ dunghead.js
@@ -887,6 +887,8 @@ window.onload = function () {
 
 				while (true) {
 					let nextCardElement = await handCardClick(player);
+					const nextCardIndex = remainingCards.indexOf(nextCardElement.card);
+					remainingCards.splice(nextCardIndex, 1);
 
 					// If the next selected card is the same rank as the first selected
 					if (nextCardElement.card.rank === selectedCard.rank) {
@@ -895,17 +897,17 @@ window.onload = function () {
 							selectedCardElements.push(nextCardElement);
 							selectedCards.push(nextCardElement.card);
 							remainingCards.splice(selectedCard, 1)
-
-							// Exit the loop if none of the cards in remainingCards have the same rank as selectedCard
-							if (!remainingCards.some(card => card.rank === selectedCard.rank)) {
-								break;
-							}
 						} else {
 							break; // Exit the loop if the player selects a card they have already chosen
 						}
 					} else {
 						appendToGameLog("Next selected card must be the same rank as the first selected card.");
 						break; // Exit the loop if the player selects a card with a different rank
+					}
+
+					// Exit the loop if none of the cards in remainingCards have the same rank as selectedCard
+					if (!remainingCards.some(card => card.rank === selectedCard.rank)) {
+						break;
 					}
 				}
 			}
