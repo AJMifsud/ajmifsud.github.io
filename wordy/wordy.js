29d56504ca8b80e2e17061f2c2b00aa9204979b4
Andy <milfsud@gmail.com>
2023-10-08 14:53:10 +0100

    Added backspace physical key input

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -9,76 +9,88 @@ window.onload = function () {
 
 	function waitForInput(eventType, element) {
 		return new Promise(resolve => {
-		  function eventHandler(event) {
-			resolve(event);
-			element.removeEventListener(eventType, eventHandler);
-		  }
-		  element.addEventListener(eventType, eventHandler);
+			function eventHandler(event) {
+				resolve(event);
+				element.removeEventListener(eventType, eventHandler);
+			}
+			element.addEventListener(eventType, eventHandler);
 		});
-	  }
+	}
 
-	  startButton.addEventListener("click", async function () {
+	startButton.addEventListener("click", async function () {
 		startButton.style.display = "none";
 		keyboard.style.display = "grid";
 		let word = "dog";
 		let letters = splitWord(word);
-	  
+
 		for (const guess of guesses) {
-		  const guessLetters = guess.querySelectorAll('.letter');
-	  
-		  for (let i = 0; i < guessLetters.length; i++) {
-			const letter = guessLetters[i];
-			const event = await Promise.race([
-			  waitForInput('keydown', document),
-			  waitForInput('click', startButton)
-			]);
-	  
-			if (event.type === 'keydown') {
-				const key = event.key;
-			  
-				// Check if the key is a character (a-z) and convert to uppercase
-				if (key.match(/^[a-zA-Z]$/)) {
-				  const newText = key.toUpperCase();
-				  letter.textContent = newText;
+			const guessLetters = guess.querySelectorAll('.letter');
+			let i = 0;
+
+			while (i < guessLetters.length) {
+				const event = await Promise.race([
+					waitForInput('keydown', document),
+					waitForInput('click', startButton)
+				]);
+
+				if (event.type === 'keydown') {
+					const key = event.key;
+
+					if (key.match(/^[a-zA-Z]$/)) {
+						const newText = key.toUpperCase();
+						guessLetters[i].textContent = newText;
+						i++;
+					} else if (key === 'Backspace') {
+						guessLetters[i].textContent = '';
+						if (i > 0) {
+							i--;
+						}
+					}
 				}
-			  }
-		  }
+				if (i == guessLetters.length) {
+					if (i > 0) {
+						i--;
+					}
+				}
+
+			}
 		}
-	  });
-	
+
+	});
+
 
 	plus.addEventListener("click", function () {
-		if (wordLength.value <= 20){
-		wordLength.value++;
+		if (wordLength.value <= 20) {
+			wordLength.value++;
 
-        guesses.forEach(guess => {
-            const letter = document.createElement('div');
-            letter.className = 'letter';
-            guess.appendChild(letter);
-        });
+			guesses.forEach(guess => {
+				const letter = document.createElement('div');
+				letter.className = 'letter';
+				guess.appendChild(letter);
+			});
 
-		startButton.style.display = "flex";
-		keyboard.style.display = "none";
-	}
+			startButton.style.display = "flex";
+			keyboard.style.display = "none";
+		}
 	});
 
 	minus.addEventListener("click", function () {
-		if (wordLength.value >= 4){
-		wordLength.value--;
+		if (wordLength.value >= 4) {
+			wordLength.value--;
 
-        guesses.forEach(word => {
-            const letters = word.querySelectorAll('.letter');
-            const lastLetter = letters[letters.length - 1];
-            	word.removeChild(lastLetter);
-        });
-		
-		startButton.style.display = "flex";
-		keyboard.style.display = "none";
-	}
+			guesses.forEach(word => {
+				const letters = word.querySelectorAll('.letter');
+				const lastLetter = letters[letters.length - 1];
+				word.removeChild(lastLetter);
+			});
+
+			startButton.style.display = "flex";
+			keyboard.style.display = "none";
+		}
 	});
 
 	function splitWord(word) {
 		const letters = word.toUpperCase().split('');
 		return letters;
-	  }
+	}
 }
